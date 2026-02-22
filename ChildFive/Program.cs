6215b4e90f656f99de810e74fcd08abd32e51ef8
DvlCbm <russeljeoff2@gmail.com>
2026-02-23 00:38:57 +0800

    Add files via upload

---
+++ ChildFive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+class TechSupportTicket
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your device name: ");
+        string device = Console.ReadLine();
+
+        Console.Write("Enter the brand of your device: ");
+        string brand = Console.ReadLine();
+
+        Console.Write("Enter the model year of your device: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        string ticketID = brand + year;
+
+        Console.WriteLine($"Hello! Your {brand} {device} has been logged. Your support ID is {ticketID} -HELP.");
+
+
+        Console.ReadKey();
+    }
+}
