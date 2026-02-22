1db57c2631ddb3591e273fdae1416d41ed567f5c
DvlCbm <russeljeoff2@gmail.com>
2026-02-22 23:24:11 +0800

    Add files via upload

---
+++ ChildNine/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+    public static int Gwa(int sum, int attmp)
+    {
+        return sum / attmp;
+    }
+    public static void Main(string[] args)
+    {
+
+        int summing = 0;
+        int grade;
+        int subject = 1;
+        int attempt = -1; //i put -1 instead of 0 to decrease the divisor avoid the 0 input from being included in the getting the average
+
+        do
+        {
+            Console.Write($"Enter grade #{subject} (enter '0' to stop): ");
+            grade = Convert.ToInt32(Console.ReadLine());
+
+            summing += grade;
+            attempt++;
+            subject++;
+
+        }while (grade != 0);
+           int GWA = Gwa(summing, attempt);
+           Console.WriteLine($"your GWA is {GWA}%");
+
+        string status;
+        if (GWA >= 75)
+        {
+            status = "Passed";
+        }
+        else
+        {
+            status = "Failed!";
+        }
+
+        Console.WriteLine($"Status: {status}");
+
+            Console.ReadKey();
+
+    }
+}
