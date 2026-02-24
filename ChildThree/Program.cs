e5fdded99f1b5f239d737a2bcd2c4b5aaa555fb5
DvlCbm <russeljeoff2@gmail.com>
2026-02-24 23:09:42 +0800

    Add files via upload

---
+++ ChildThree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("first num: ");
+        int Fnum = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("second num");
+        int Snum = Convert.ToInt32(Console.ReadLine());
+
+        int Sum = Fnum + Snum;
+
+        Console.WriteLine($"Sum: {Sum}");
+
+        Console.ReadKey();
+    }
+}
