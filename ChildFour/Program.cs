5a3183b55676fac33f78a0b71947253334a56dc8
DvlCbm <russeljeoff2@gmail.com>
2026-02-24 23:10:11 +0800

    Add files via upload

---
+++ ChildFour/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class GamerProfile
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your fist name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Enter your current level: ");
+        int level = Convert.ToInt32(Console.ReadLine());
+ 
+        int nextLevel = level + 1;
+
+        Console.WriteLine($"Profile Created: {lastName}, {firstName}. Current Rank: {level}. Get ready for level {nextLevel}!");
+
+
+
+        Console.ReadKey();
+    }
+}
