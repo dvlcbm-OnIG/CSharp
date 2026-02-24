0fcb83b605d806090298eff1ad1f69b8b275daaa
DvlCbm <russeljeoff2@gmail.com>
2026-02-24 23:13:21 +0800

    Add files via upload

---
+++ ChildSix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+class SmartSavingsTracker
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Username: ");
+        string userName = Console.ReadLine();
+
+        Console.Write("Enter your current balance: ");
+        int currentBalance = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter your monthly goal: ");
+        int monthlyGoal = Convert.ToInt32(Console.ReadLine());
+
+        int futureBalance = currentBalance + monthlyGoal;
+
+        Console.WriteLine($"User: {userName} Current Balance: ${currentBalance}. If you hit your goal, your new balance will be ${futureBalance}!");
+
+
+        Console.ReadKey();
+    }
+}
