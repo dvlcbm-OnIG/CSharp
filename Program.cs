b74b5b0e81de224c190ff1031d10e450e030994d
DvlCbm <russeljeoff2@gmail.com>
2026-03-04 22:09:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class RpgInventoryCheck
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter character name: ");
+        string characterName = Console.ReadLine();
+
+        Console.Write("Enter item name: ");
+        string itemName = Console.ReadLine();
+
+        Console.Write("Enter curent item amount: ");
+        int currentAmount = Convert.ToInt32(Console.ReadLine());
+
+        int newAmount = currentAmount + 5;
+
+        Console.WriteLine($"{characterName}, you found a chest! You now have {newAmount} {itemName}. Keep going!");
+
+        Console.ReadKey();
+    }
+}
