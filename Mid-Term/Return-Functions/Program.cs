7e9748f05bf5e8cd0e38cc7baf50ce41ba64ff44
DvlCbm <russeljeoff2@gmail.com>
2026-03-16 22:26:57 +0800

    Add files via upload

---
+++ Mid-Term/Return-Functions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Program
+{
+    //get the average of four numbers using void method
+
+    public static double AverageOfFour(double a, double b, double c, double d)
+    {
+        double result = (a + b + c + d) / 4; //calculations
+        return result; //return the result to the function
+    }
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(AverageOfFour(75, 75, 75, 75)); //calling the function and giving it values
+
+        Console.ReadKey();
+    }
+}
