0d9a89d12398b40050d39d71a6099ea48f8bcfc3
DvlCbm <russeljeoff2@gmail.com>
2026-03-25 23:34:11 +0800

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public class Parent
+{
+    public double Circle(double radius)
+    {
+        return Math.PI * (radius * radius);
+    }
+}
+
+public class Child : Parent
+{
+    public void Introduce()
+    {
+        Console.WriteLine(Circle(10));
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Child child = new Child();
+
+
+        child.Introduce();
+    }
+}
